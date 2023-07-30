902db48e887d5e0878d7192d2a4c5d42935ae1c6
novodias <nil.diasbessa@gmail.com>
2023-07-30 14:07:57 -0300

    Change: use container class / New: create room

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -21,15 +21,33 @@ const HomePage = () => {
         }
     }
 
+    const createRoom = async () => {
+        const response = await fetch("http://localhost:3001/api/room/create");
+        const { id } = await response.json();
+        navigate(`room/${id}`);
+    }
+
     return (
-        <div className='home-join-container'>
-            <label htmlFor='join-room-input'>Join a room</label>
-            <div>
+        <div className='home-container'>
+            <div className='col container join-container'>
+                <h2>Join a room</h2>
+                <label htmlFor='join-room-input'>Room code</label>
+                <h3>Insert the code below</h3>
                 <input id='join-room-input' value={room}
-                    placeholder='Room code...'
+                    placeholder='Ex.: ABC12345' type='text'
                     onInput={_onInput}
                     onKeyUp={_onKeyUp} />
                 <button className="btn" onClick={redirectPage}>Join</button>
+            </div>
+            <div className='col container'>
+                <h2>Create a room</h2>
+                <label htmlFor="create-room-input">Name</label>
+                <h3>Insert the room's name below</h3>
+                <input type='text' id='create-room-input'/>
+                <label htmlFor="create-room-password-input">Password</label>
+                <h3>Insert the room's password below</h3>
+                <input type='text' id='create-room-password-input'/>
+                <button className='btn' onClick={createRoom}>Create</button>
             </div>
         </div>
     );
