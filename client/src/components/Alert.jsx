d3e9386136b992073b92bcd77d7df01a70a2bab0
novodias <nil.diasbessa@gmail.com>
2023-07-31 12:37:54 -0300

    New: add style paramter to Alert.jsx

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { CheckRounded, CloseRounded } from '@mui/icons-material'
 
-export default function Alert({ message, type }) {
+export default function Alert({ message, type, style }) {
 
     return (
-        <div className={`alert alert-${type}`}>
+        <div className={`alert alert-${type}`} style={style}>
             <span>{message}</span>
             <div className='icon-container'>
                 { type === 'success' ? <CheckRounded /> : null}
