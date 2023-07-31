683d5656e815b089fcce950f6eb6147df80ddba5
novodias <nil.diasbessa@gmail.com>
2023-07-31 00:03:14 -0300

    New: checkbox to add password in create room

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -5,9 +5,12 @@ import crypto from 'crypto-js';
 
 const HomePage = () => {
 
+    // join
     const [room, setRoom] = useState('');
     
+    // create
     const [name, setName] = useState('');
+    const [hasPass, setHasPass] = useState(false);
     const [password, setPassword] = useState('');
 
     let navigate = useNavigate();
@@ -26,15 +29,24 @@ const HomePage = () => {
         }
     }
 
+    const _onChangeCheckBoxPassword = (e) => {
+        const checked = e.target.checked;
+        setHasPass(checked);
+    }
+
     const createRoom = async () => {
+        let roomName;
         if (!name || name === '') {
-            setName(`${sessionStorage.getItem('nickname')}'s room`);
+            // setName(`${sessionStorage.getItem('nickname')}'s room`);
+            roomName = `${sessionStorage.getItem('nickname')}'s room`;
+        } else {
+            roomName = name;
         }
 
-        const passwordHash = crypto.MD5(password).toString();
+        const passwordHash = hasPass ? crypto.MD5(password).toString() : null;
         const response = await fetch("http://localhost:3001/api/room", {
             method: "POST",
-            body: JSON.stringify({ name, passwordHash }),
+            body: JSON.stringify({ name: roomName, passwordHash, isPrivate: false }),
             headers: {
                 "Content-Type": "application/json"
             },
@@ -42,9 +54,10 @@ const HomePage = () => {
         
         const { id } = await response.json();
 
-        sessionStorage.setItem("RoomPasswordHash", passwordHash);
-        
-        // password here
+        if (hasPass === true) {
+            sessionStorage.setItem("RoomPasswordHash", passwordHash);
+        }
+
         navigate(`room/${id}`);
     }
 
@@ -56,7 +69,7 @@ const HomePage = () => {
                 <h3>Insert the code below</h3>
                 <input id='join-room-input' value={room}
                     placeholder='Ex.: ABC12345' type='text'
-                    onInput={_onInput}
+                    onInput={_onInput} autoComplete='off'
                     onKeyUp={_onKeyUp} />
                 <button className="btn" onClick={redirectPage}>Join</button>
             </div>
@@ -64,12 +77,22 @@ const HomePage = () => {
                 <h2>Create a room</h2>
                 <label htmlFor="create-room-input">Name</label>
                 <h3>Insert the room's name below</h3>
-                <input type='text' id='create-room-input'
-                    value={name} onInput={(e) => setName(e.target.value)} />
-                <label htmlFor="create-room-password-input">Password</label>
-                <h3>Insert the room's password below</h3>
-                <input type='password' id='create-room-password-input'
-                    value={password} onInput={(e) => setPassword(e.target.value)} />
+                <input type='text' id='create-room-input' placeholder={`${sessionStorage.getItem("nickname")}'s room`}
+                    value={name} onInput={(e) => setName(e.target.value)} autoComplete='off' />
+                
+                <div className='row' style={{ marginTop: '20px', alignItems: 'center' }}>
+                    <input type="checkbox" id='checkbox-has-password' value={hasPass} onChange={_onChangeCheckBoxPassword} />
+                    <label htmlFor='checkbox-has-password' style={{margin: '0', flexGrow: '1'}}>Use a password</label>
+                </div>
+                
+                {hasPass &&
+                <>
+                    <label htmlFor="create-room-password-input">Password</label>
+                    <h3>Insert the room's password below</h3>
+                    <input type='password' id='create-room-password-input' autoComplete='off'
+                        value={password} onInput={(e) => setPassword(e.target.value)} />
+                </>}
+                
                 <button className='btn' onClick={createRoom}>Create</button>
             </div>
         </div>
