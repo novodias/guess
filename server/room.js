b25817c092b25a386ae64487b2c6207acfd92d1b
novodias <nil.diasbessa@gmail.com>
2023-08-03 02:33:34 -0300

    Create a uuid for the owner /  handle ws messages / fix minor issues

--- server/room.js
+++ server/room.js
@@ -1,7 +1,8 @@
+// const { WebSocket } = require('ws');
 const Player = require("./player");
+const uuid = require('uuid');
 
 class Room {
-
     /**
      * 
      * @param {String} id
@@ -12,12 +13,17 @@ class Room {
     constructor(id, name, passwordHash, isPrivate) {
         this.id = id;
         this.name = name;
+        this.isPrivate = isPrivate;
         this.passwordHash = passwordHash || null;
-        this.isPrivate = isPrivate === true;
         this.hasPassword = passwordHash !== null;
+        this.ownerId = uuid.v4();
         
         this.players = new Map();
         this.listeners = {};
+
+        // todo: add a timer to start the game
+        // todo: get random songs
+        // todo: kicked players (don't allow back in)
     }
 
     getSize() {
@@ -50,18 +56,101 @@ class Room {
 
     /**
      * 
+     * @param {Number} id 
+     * @returns {Player | undefined}
+     */
+    _getPlayer(id) { return this.players.get(id) }
+
+    /**
+     * 
+     * @param {*} message 
+     * @param {Player} player 
+     */
+    _handleMessage(message, player) {
+        const type = message.type;
+        const body = message.body;
+
+        console.log(message);
+        
+        const handler = {
+            "kick": () => {
+                const isOwner = body.owner === this.ownerId;
+
+                if (!isOwner) {
+                    return;
+                }
+
+                // console.log("kick:", body);
+                this.removePlayer(body.id, 3000, "You got kicked from the room.");
+            },
+
+            "submit": () => {
+                const id = body.id;
+                const title_id = body.title_id;
+
+                // todo: do the rest of the game lol
+                // gonna need to verify title_id answer with the randomly selected
+
+                const player = this.players.get(id);
+
+                // the player class emits a change event and broadcasts to all
+                player.setPoints(player.points + 15);
+            },
+
+            "chat": () => {
+                // self explanatory
+                this.broadcast({
+                    type: "chat",
+                    body: { text: body.text, nickname: player.nickname }
+                });
+            }
+        }
+
+        handler[type]();
+    }
+
+    /**
+     * 
+     * @param {MessageEvent} e 
+     * @returns {*}
+     */
+    parse(e) {
+        return JSON.parse(e.data.toString());
+    }
+
+    /**
+     * 
      * @param {Player} player 
      */
     addPlayer(player) {
-        player.ws.on("close", ws => {
-            this.removePlayer(player.id);
+        player.ws.on("close", () => {
+            console.log(`[Room/${this.id}] ${player.id}/${player.nickname} exited the room`);
+            
+            if (this.players.has(player.id)) {
+                this.removePlayer(player.id);
+            }
         });
+
+        player.ws.onmessage = (e) => {
+            const message = this.parse(e);
+            this._handleMessage(message, player);
+        }
+
+        player.onchange(body => {
+            // broadcast changes to all;
+            this.broadcast(body);
+        })
 
         this.players.set(player.id, player);
 
-        const joined = {
-            type: "joined",
-            body: player.getPlayerData(),
+        // const joined = {
+        //     type: "joined",
+        //     body: player.getPlayerData(),
+        // };
+
+        const you = {
+            type: "yourid",
+            body: { id: player.id }
         };
 
         const players = {
@@ -71,6 +160,9 @@ class Room {
 
         // sends to player who's joined all the players
         // this.send(players, player);
+        
+        // send id to the player
+        this.send(you, player);
         
         // sends to all players the person who's joined
         this.broadcast(players);
@@ -124,14 +216,18 @@ class Room {
         player.send(object);
     }
 
-    removePlayer(id) {
-        this.players.get(id).closeWebSocket();
+    removePlayer(id, code = undefined, reason = undefined) {
+        const player = this._getPlayer(id);
+        player && player.closeWebSocket(code, reason);
+
         this.players.delete(id);
 
         const size = this.getSize();
         if (size === 0 && typeof this.listeners["empty"] === 'function') {
+            // emits empty and then the cluster deletes the room
             this.emit("empty", this.id);
         } else {
+            // broadcast player that exited
             const message = {
                 type: "exited",
                 body: { id }
