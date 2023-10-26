3d98024ca0054937270af29564c652b8ec872924
novodias <nil.diasbessa@gmail.com>
2023-10-26 18:14:30 -0300

    Fixed button not updating if prop.play is true

--- client/src/components/room/AudioPlayer.jsx
+++ client/src/components/room/AudioPlayer.jsx
@@ -21,15 +21,22 @@ function refCallback (ref, callback) {
 
 let x = 0;
 
-export default function AudioPlayer({ src, play, startTime, canvasCallback }) {
+export default function AudioPlayer({ src, play, startTime, playButtonDisabled, canvasCallback }) {
     const audioRef = useRef(new Audio());
     const audioCtxRef = useRef(new AudioContext());
+    /**
+     * @type {import("react").MutableRefObject<MediaElementAudioSourceNode>}
+     */
     const audioSource = useRef(null);
+    /**
+     * @type {import("react").MutableRefObject<AnalyserNode>}
+     */
     const analyser = useRef(null);
 
     const rafRef = useRef(null);
     const progressRef = useRef(null);
     const [playState, setPlayState] = useState('none');
+    const [state, setState] = useState(0);
     const [volume, setVolume] = useState(audioRef.current.volume * 100);
     const [muted, setMuted] = useState(audioRef.current.muted);
 
@@ -50,51 +57,70 @@ export default function AudioPlayer({ src, play, startTime, canvasCallback }) {
                 analyser.current.fftSize = 128;
             }
         });
+
+        return () => {
+            console.log("audioSource and analyser disconnected and disposed");
+            if (audioSource.current) {
+                audioSource.current.disconnect();
+                audioSource.current = null;
+            }
+
+            if (analyser.current) {
+                analyser.current.disconnect();
+                analyser.current = null;
+            }
+
+            if (audioRef.current) {
+                audioRef.current = null;
+            }
+        }
     }, []);
     
     useEffect(() => {
-        canvasCallback((canvas) => {
-            const ctx = canvas.getContext("2d");
-            const drawVisualizer = ({
-                bufferLength,
-                dataArray,
-                barWidth
-            }) => {
-                let barHeight;
-                for (let i = 0; i < bufferLength; i++) {
-                    barHeight = dataArray[i];
-                    // const red = (i * barHeight) / 10;
-                    // const green = i * 4;
-                    // const blue = barHeight / 4 - 12;
-                    ctx.fillStyle = `rgba(0, 0, 0, 0.3)`;
-                    ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-                    x += barWidth * 1.2;
+        if (canvasCallback) {
+            canvasCallback((canvas) => {
+                const ctx = canvas.getContext("2d");
+                const drawVisualizer = ({
+                    bufferLength,
+                    dataArray,
+                    barWidth
+                }) => {
+                    let barHeight;
+                    for (let i = 0; i < bufferLength; i++) {
+                        barHeight = dataArray[i];
+                        // const red = (i * barHeight) / 10;
+                        // const green = i * 4;
+                        // const blue = barHeight / 4 - 12;
+                        ctx.fillStyle = `rgba(0, 0, 0, 0.3)`;
+                        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+                        x += barWidth * 1.2;
+                    }
+                };
+        
+                const node = getAnalyser();
+                const bufferLength = node.frequencyBinCount;
+                const dataArray = new Uint8Array(bufferLength);
+                const barWidth = canvas.width / bufferLength;
+                let frameId;
+                
+                function animate() {
+                    x = 0;
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
+                    node.getByteFrequencyData(dataArray);
+                    drawVisualizer({
+                        bufferLength, dataArray, barWidth
+                    });
+        
+                    frameId = requestAnimationFrame(animate);
                 }
-            };
-    
-            const node = getAnalyser();
-            const bufferLength = node.frequencyBinCount;
-            const dataArray = new Uint8Array(bufferLength);
-            const barWidth = canvas.width / bufferLength;
-            let frameId;
-            
-            function animate() {
-                x = 0;
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                node.getByteFrequencyData(dataArray);
-                drawVisualizer({
-                    bufferLength, dataArray, barWidth
-                });
-    
-                frameId = requestAnimationFrame(animate);
-            }
-    
-            animate();
-            return () => {
-                console.log("canceled frameid audio visualizer raf");
-                cancelAnimationFrame(frameId);
-            };
-        })
+        
+                animate();
+                return () => {
+                    console.log("canceled frameid audio visualizer raf");
+                    cancelAnimationFrame(frameId);
+                };
+            })
+        }
     }, [canvasCallback]);
 
     /**
@@ -133,25 +159,6 @@ export default function AudioPlayer({ src, play, startTime, canvasCallback }) {
         })
     }
 
-    /**
-     * 
-     * @returns {Number|null}
-     */
-    const getPercentage = useCallback(() => {
-        let percent = null;
-        audioCallback((audio) => {
-            percent = (audio.currentTime / audio.duration).toFixed(5);
-        });
-        return percent;
-    }, []);
-
-    const updateProgressStyle = useCallback(() => {
-        const percent = getPercentage() || 0;
-        progressInvoke((prog) => {
-            prog.style.left = `${percent * 100}%`;
-        });
-    }, [getPercentage]);
-
     const setSource = useCallback((src) => audioCallback((audio) => {
         audio.src = src;
     }), []);
@@ -165,16 +172,38 @@ export default function AudioPlayer({ src, play, startTime, canvasCallback }) {
         audioCallback((audio) => {
             audio.muted = true;
             setMuted(audio.muted);
+
+            /**
+             * 
+             * @returns {Number|null}
+             */
+            const getPercentage = () => {
+                let percent = 0;
+                audioCallback((audio) => {
+                    percent = (audio.currentTime / audio.duration).toFixed(5);
+                });
+                return percent;
+            };
+
+            const updateProgress = () => {
+                const percent = getPercentage();
+                progressInvoke((prog) => {
+                    prog.style.left = `${percent * 100}%`;
+                });
+                rafRef.current = requestAnimationFrame(updateProgress);
+            };
             
             audio.onplay = (e) => {
                 setPlayState('play');
-                rafRef.current = requestAnimationFrame(updateProgressStyle);
+                updateProgress();
                 audioCtxRef.current.resume();
             };
 
             function cancelProgressRaf(calledOn = '') {
-                cancelAnimationFrame(rafRef.current);
-                console.log("canceled audio progress raf", calledOn);
+                if (rafRef.current) {
+                    cancelAnimationFrame(rafRef.current);
+                    console.log("canceled audio progress raf", calledOn);
+                }
             }
             
             audio.onpause = (e) => {
@@ -191,7 +220,7 @@ export default function AudioPlayer({ src, play, startTime, canvasCallback }) {
             cancelAnimationFrame(rafRef.current);
             console.log("canceled audio progress raf");
         }
-    }, [updateProgressStyle]);
+    }, []);
 
     useEffect(() => {
         if (src) {
@@ -205,30 +234,45 @@ export default function AudioPlayer({ src, play, startTime, canvasCallback }) {
         }
     }, [play, playAudio]);
 
-    function PlaybackState() {
-        let state = 0;
+    const playbackState = useCallback(() => {
+        // let state = 0;
         audioCallback((audio) => {
             const readyState = audio.readyState;
             // (cannot play)
             if (readyState === HTMLMediaElement.HAVE_NOTHING) {
-                state = 0;
-            } else if (readyState === HTMLMediaElement.HAVE_CURRENT_DATA || 
+                // state = 0;
+                setState(0);
+            } else if (readyState === HTMLMediaElement.HAVE_CURRENT_DATA ||
                 readyState === HTMLMediaElement.HAVE_METADATA) {
-                state = 1;
+                // state = 1;
+                setState(1);
             } else {
-                state = 2;
+                // state = 2;
+                setState(2);
             }
         });
-        return state;
-    }
+        // return state;
+    }, []);
+
+    useEffect(() => {
+        if (playState !== 'none') {
+            // console.log("playbackState called, playState:", playState);
+            playbackState();
+        }
+    }, [playState, playbackState]);
+
 
     function PlaybackIcon() {
-        const playbackState = PlaybackState();
+        // const playbackState = PlaybackState();
 
         let icon = null;
-        switch (playbackState) {
+        switch (state) {
             case 0:
-                icon = <PlayArrowRounded />;
+                if (playState === 'pause') {
+                    icon = <PlayArrowRounded />;
+                } else {
+                    icon = <PauseRounded />;
+                }
                 break;
             
             case 1:
@@ -253,13 +297,10 @@ export default function AudioPlayer({ src, play, startTime, canvasCallback }) {
 
     function onClickPlayControl() {
         audioCallback((audio) => {
-            const state = PlaybackState();
-            if (state === 2) {
-                if (playState === 'play') {
-                    audio.pause();
-                } else {
-                    audio.play();
-                }
+            if (playState === 'play') {
+                audio.pause();
+            } else {
+                audio.play();
             }
         });
     }
@@ -302,7 +343,7 @@ export default function AudioPlayer({ src, play, startTime, canvasCallback }) {
 
     return (
         <div id="audio-ui">
-            <button onClick={onClickPlayControl} disabled>
+            <button onClick={onClickPlayControl} disabled={playButtonDisabled}>
                 <PlaybackIcon />
             </button>
             <div className="progress-wrapper">
