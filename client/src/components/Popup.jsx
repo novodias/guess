beb3fdfdb2535d1ef646d44a09edaaf97efdce42
novodias <nil.diasbessa@gmail.com>
2023-10-30 15:43:20 -0300

    Added end animation

--- client/src/components/Popup.jsx
+++ client/src/components/Popup.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, useCallback } from 'react';
 import './Popup.css';
 import { usePopupDispatchContext } from '../context/PopupProvider';
 
@@ -16,7 +16,7 @@ import { usePopupDispatchContext } from '../context/PopupProvider';
  * 
  */
 export default function Popup({
-    text, orient, gap, batchNumber, close, uid,
+    text, orient, gap, batchNumber, uid,
     hasButton, buttonText, onButtonClick, /** onDone,*/
     waitForClick }) {
     /**
@@ -33,7 +33,8 @@ export default function Popup({
     const buttonClick = (e) => {
         if (onButtonClick) {
             onButtonClick(e);
-            remove(uid);
+            AnimationEnd();
+            // remove(uid);
         }
     }
 
@@ -48,58 +49,107 @@ export default function Popup({
             </button>
         )
     }
-    
-    useEffect(() => {
-        function easeOutCubic(num) {
-            return 1 - Math.pow(1 - num, 3);
-        }
-    
-        const setPopupPos = (value) => {
-            popupRef.current.style[orient] = value + 'px';
-        }
 
-        const tick = () => {
+    function easeOutCubic(num) {
+        return 1 - Math.pow(1 - num, 3);
+    }
+
+    const setPopupPos = useCallback((value) => {
+        popupRef.current.style[orient] = value + 'px';
+    }, [orient]);
+
+    const popupDisplayNone = useCallback(() => {
+        popupRef.current.style.display = 'none';
+    }, []);
+
+    const Animate = useCallback(({ wishPos, finalPos, elapsed }, onNextFrame, onEnd) => {
+        if (elapsed < duration) {
+            setPopupPos(wishPos);
+            rafRef.current = requestAnimationFrame(onNextFrame);
+        } else {
+            if (wishPos !== finalPos) {
+                setPopupPos(finalPos);
+            }
+            
+            cancelAnimationFrame(rafRef.current);
+            onEnd();
+            // doesnt expect a click, sets a timer to call ondone
+            // if (waitForClick === false) {
+            //     setTimeout(() => remove(uid), 1000 * 7);
+            // }
+        }
+    }, [setPopupPos]);
+
+    const AnimationEnd = useCallback(() => {
+        startTimeRef.current = Date.now();
+        
+        const end = () => {
             const elapsed = Date.now() - startTimeRef.current;
+            const remaining = duration - elapsed;
             const { height } = popupRef.current.getBoundingClientRect();
             const finalPos = height * batchNumber + gap * batchNumber;
-            const percent = elapsed / duration;
+            const percent = remaining / duration;
             const wishPos = finalPos * easeOutCubic(percent);
-            
-            if (elapsed < duration) {
-                setPopupPos(wishPos);
-                rafRef.current = requestAnimationFrame(tick);
-            } else {
-                if (wishPos !== finalPos) {
-                    setPopupPos(finalPos);
-                }
-                
-                cancelAnimationFrame(rafRef.current);
+            console.log("end:", wishPos);
     
-                // doesnt expect a click, sets a timer to call ondone
-                if (waitForClick === false) {
-                    setTimeout(() => remove(uid), 1000 * 7);
-                }
-            }
+            Animate({ wishPos, finalPos, elapsed }, end, () => {
+                popupDisplayNone();
+                remove(uid)
+            });
         }
 
+        end();
+    }, [Animate, batchNumber, gap, remove, uid, popupDisplayNone]);
+
+    const AnimationStart = useCallback(() => {
+        const elapsed = Date.now() - startTimeRef.current;
+        const { height } = popupRef.current.getBoundingClientRect();
+        const finalPos = height * batchNumber + gap * batchNumber;
+        const percent = elapsed / duration;
+        const wishPos = finalPos * easeOutCubic(percent);
+        
+        Animate({ wishPos, finalPos, elapsed }, AnimationStart, () => {
+            if (waitForClick === false) {
+                setTimeout(AnimationEnd, 1000 * 7);
+            }
+        });
+    }, [Animate, AnimationEnd, batchNumber, gap, waitForClick]);
+    
+    useEffect(() => {
+        // const tick = () => {
+        //     const elapsed = Date.now() - startTimeRef.current;
+        //     const { height } = popupRef.current.getBoundingClientRect();
+        //     const finalPos = height * batchNumber + gap * batchNumber;
+        //     const percent = elapsed / duration;
+        //     const wishPos = finalPos * easeOutCubic(percent);
+        //     if (elapsed < duration) {
+        //         setPopupPos(wishPos);
+        //         rafRef.current = requestAnimationFrame(tick);
+        //     } else {
+        //         if (wishPos !== finalPos) {
+        //             setPopupPos(finalPos);
+        //         }
+        //         cancelAnimationFrame(rafRef.current);
+        //         // doesnt expect a click, sets a timer to call ondone
+        //         if (waitForClick === false) {
+        //             setTimeout(() => remove(uid), 1000 * 7);
+        //         }
+        //     }
+        // }
+
         if (initialLoad) {
-            tick();
+            AnimationStart();
             setInitialLoad(false);
             
             if (waitForClick) {
                 if (!hasButton) {
-                    popupRef.current.onclick = () => remove(uid);
+                    // popupRef.current.onclick = () => remove(uid);
+                    popupRef.current.onclick = AnimationEnd;
                     popupRef.current.style.cursor = "pointer";
                 }
             }
         }
-    }, [initialLoad, waitForClick, hasButton, batchNumber, gap, orient, remove, uid]);
-
-    useEffect(() => {
-        if (close) {
-            remove(uid);
-        }
-    }, [close, remove, uid]);
+    }, [initialLoad, waitForClick, hasButton, AnimationStart, AnimationEnd]);
     
     return (
         <div ref={popupRef} className='popup'>
