2e9428020b586154586b86ec09a0f56d6900f496
novodias <nil.diasbessa@gmail.com>
2023-11-07 19:10:58 -0300

    Refactor room route

--- server/routes/room.js
+++ server/routes/room.js
@@ -3,6 +3,7 @@ const Room = require('../room');
 const router = express.Router();
 const { nullOrUndefined } = require('../utils');
 const AbortMessage = require('../models/abortError');
+const AbortError = require('../models/abortError');
 
 /**
  * @param {Room} room
@@ -12,55 +13,40 @@ const ensureRoomAuthentication = (room, hash, res) => {
     if (room.hasPassword && nullOrUndefined(hash)) {
         res.json(room.public);
         console.log(`[Rooms/${room.id}] Room with password found, password not inserted`);
-        return false;
     }
 
     if (room.passwordHash !== hash) {
         console.log(`[Rooms/${room.id}] Room found, wrong authentication`);
-        const abt = new AbortMessage("Room's password doesn't match", 400, room.public);
-        res.abort(abt);
-        return false;
+        throw new AbortMessage("Room's password doesn't match", 400, room.public);
     }
-
-    return true;
 }
 
 router.use("/", (req, res, next) => {
-    if (req.method === 'GET') {
-        const { id } = req.query;
-        const authorization = req.headers.authorization;
-        const hash = Buffer.from(authorization, 'base64').toString('ascii');
-        /**
-         * @type {Room}
-         */
-        const room = req.cluster.getRoom(id);
-        // console.log(room.passwordHash, hash, room.passwordHash === hash);
-
-        if (!room) {
-            console.log("[Rooms] Couldn't found room", id);
-            res.abort(new AbortMessage("Room not found", 404));
-            return;
-        }
-
-        if (!room.hasPassword) {
+    try {
+        if (req.method === 'GET') {
+            const { id } = req.query;
+            const authorization = req.headers.authorization;
+            const hash = Buffer.from(authorization, 'base64').toString('ascii');
+            const room = req.cluster.getRoom(id);
             req.room = room;
-            console.log(`[Rooms/${room.id}] Room found, password not needed`);
-            next()
-            return;
+    
+            if (!room) {
+                console.log("[Rooms] Couldn't found room", id);
+                throw new AbortMessage("Room not found", 404);
+            }
+            
+            if (room.hasPassword) {
+                ensureRoomAuthentication(room, hash, res);
+            }
         }
-
-        if (!ensureRoomAuthentication(room, hash, res)) {
-            return;
-        }
-
-        req.room = room;
+    
+        next();
+    } catch (err) {
+        next(err);
     }
-
-    next();
 })
 
 router.get("/", (req, res) => {
-    // auth part gone
     /**
      * @type {Room}
      */
@@ -68,20 +54,24 @@ router.get("/", (req, res) => {
     res.json(room.private);
 });
 
-router.post("/", async (req, res) => {
-    const content_type = req.get("Content-Type");
-    if (content_type && content_type !== "application/json") {
-        res.status(406).send("Not acceptable");
+router.post("/", async (req, res, next) => {
+    try {
+        const content_type = req.get("Content-Type");
+        if (!content_type || content_type !== "application/json") {
+            throw new AbortError("Content type not acceptable", 406);
+        }
+    
+        const { name, passwordHash, isPrivate } = req.body;
+        const db = req.db;
+        const cluster = req.cluster;
+    
+        const songs = await db.get_songs_random(10);
+        const room = cluster.createRoom(name, passwordHash, isPrivate, songs);
+    
+        res.json({ id: room.id, ownerId: room.ownerId });
+    } catch (err) {
+        next(err);
     }
-
-    const { name, passwordHash, isPrivate } = req.body;
-    const db = req.db;
-    const cluster = req.cluster;
-
-    const songs = await db.get_songs_random(10);
-    const room = cluster.createRoom(name, passwordHash, isPrivate, songs);
-
-    res.json({ id: room.id, ownerId: room.ownerId });
 });
 
 module.exports = router;
