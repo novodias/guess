4a0765d853c412a357df0a19e2d528db6cc0b8ee
novodias <nil.diasbessa@gmail.com>
2023-11-27 17:48:24 -0300

    feat: Modal component

---
+++ client/src/components/elements/Modal.jsx
@@ -0,0 +1,46 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useRef, MutableRefObject } from "react";
+import { createPortal } from 'react-dom'
+import './Modal.css';
+import { CloseRounded } from '@mui/icons-material'
+
+function Modal({ className, children }) {
+    /**
+     * @type {MutableRefObject<HTMLSpanElement>}
+     */
+    const modalRef = useRef(undefined);
+    const close = () => {
+        modalRef.current.remove();
+    }
+
+    return createPortal(
+        <div className={`modal${' ' + className}`} ref={modalRef}>
+            <span className="modal-close" onClick={close}>
+                <CloseRounded />
+            </span>
+            {children}
+        </div>,
+        document.body
+    )
+}
+
+function Header({ text }) {
+    return (
+        <div className="modal-header">
+            <h1>{text}</h1>
+        </div>
+    )
+}
+
+function Body({ children }) {
+    return (
+        <div className="modal-body">
+            {children}
+        </div>
+    )
+}
+
+Modal.Header = Header;
+Modal.Body = Body;
+
+export default Modal;
