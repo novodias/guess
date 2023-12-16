ff066ac96d40b592c06c40410490262d0f011b05
novodias <nil.diasbessa@gmail.com>
2023-12-16 20:33:38 -0300

    feat: VITE_APP_URL

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_APP_TITLE: string    
+    readonly VITE_APP_TITLE: string
+    readonly VITE_APP_URL: string
 }
 
 interface ImportMeta {
