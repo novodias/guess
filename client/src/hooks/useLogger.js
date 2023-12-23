527e1f41b451a52905b5f369e68c2a87194efc9a
novodias <nil.diasbessa@gmail.com>
2023-12-22 22:03:26 -0300

    change: fix css, and add parameter to objects

--- client/src/hooks/useLogger.js
+++ client/src/hooks/useLogger.js
@@ -7,19 +7,22 @@ class Logger {
 
     /**
      * @private
+     * @param {("log"|"debug"|"info")} level 
+     * @param {string} message 
      */
-    log(message, css) {
-        console.log(`%c[${this.name}]: ` + message, css);
+    log(level, message, css, obj) {
+        console[level](`%c[${this.name}]: ` + `%c${message}`, ...css, obj || '');
     }
 
-    debug(message) {
+    debug(message, ...array) {
         if (import.meta.env.DEV) {
-            this.log(message, 'color: rgb(160, 106, 160);');
+            // set this to "log" for now, for some reason chrome only shows debug on verbose
+            this.log('log', message, ['color: rgb(160, 106, 160);', 'color: reset;'], array);
         }
     }
 
     info(message) {
-        this.log(message, 'color: aquamarine; text-shadow: 1px 1px 2px black');
+        this.log('info', message, ['color: aquamarine; text-shadow: 1px 1px 2px black;', 'color: reset; text-shadow: none;']);
     }
 }
 
