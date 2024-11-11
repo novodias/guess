e6112b971ce1065759ba013cf15218dcd0a463df
Nilson Dias <nil.diasbessa@gmail.com>
2024-11-11 14:23:25 -0300

    [change] canvas audio visualizer in the middle

--- client/src/components/room/game/CanvasWorker.js
+++ client/src/components/room/game/CanvasWorker.js
@@ -7,16 +7,19 @@ export default () => {
      * @type {OffscreenCanvasRenderingContext2D | null}
      */
     let ctx = null;
+    let canvasHeight = null;
 
     const draw = ({ bufferLength, dataArray }) => {
-        let barHeight;
+        let barHeightTop, barHeightBelow;
         const barWidth = (canvas.width / bufferLength) * 3;
         let currentBar = 0;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         for (let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i] * 2;
-            ctx.fillRect(currentBar, canvas.height - barHeight, barWidth, barHeight);
+            barHeightTop = dataArray[i];
+            barHeightBelow = barHeightTop * -1;
+            ctx.fillRect(currentBar, canvasHeight - barHeightTop, barWidth, barHeightTop);
+            ctx.fillRect(currentBar, canvasHeight - barHeightBelow, barWidth, barHeightBelow);
             currentBar += barWidth * 1.2;
         }
     };
@@ -25,6 +28,7 @@ export default () => {
         const { bufferLength, dataArray, canvas: canvasMessage } = e.data;
         if (canvasMessage) {
             canvas = canvasMessage;
+            canvasHeight = canvas.height / 2;
             ctx = canvas.getContext('2d');
             // ctx.fillStyle = `black`;
             ctx.fillStyle = `rgba(0, 0, 0, 0.1)`;
